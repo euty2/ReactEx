c79732db3ad94a9895f1107cae1f99c49c0025e4
SJF05 <lakors05@gmail.com>
2025-06-13 16:48:01 +0900

    컴포넌트

--- ch04_component/src/App.js
+++ ch04_component/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 // import Child from './ex01_function/Child';
-import Child from './ex02_class/Child';
+// import Child from './ex02_class/Child';
+// import MyComponent from './ex03_props/MyComponent';
+import Library from './ex04_props/Library';
 
 function App() {
   return (
-    <Child />
+    // <Child />
+  /*
+    <>
+      <MyComponent />
+      <MyComponent name='리액트'/>
+      <MyComponent name='React'/>
+    </>
+  */
+    <>
+      <Library title='유학'/>
+      <Library title='조선 선비들의'/>
+    </>  
   );
 }
 
---
+++ ch04_component/src/ex03_props/MyComponent.jsx
@@ -0,0 +1,12 @@
+/**
+ * props는 부모 컴포넌트(App)에서 자식 컴포넌트(MyComponent)한테
+ * 데이터를 전달할 때 사용하는 객체이다.
+ * props는 다른 이름으로 변경 가능하지만, 관습적으로 props를 사용함.
+ */
+import './Child.css';
+
+function MyComponent(props){
+    return <div className="react">안녕하세요. 제 이름은 {props.name}입니다.</div>
+}
+
+export default MyComponent;
