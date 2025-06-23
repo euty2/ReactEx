bddbdda832d76536a22c19c3304d823485fc3456
SJF05 <lakors05@gmail.com>
2025-06-23 14:51:21 +0900

    링크

--- ch17_router/src/App.js
+++ ch17_router/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 // import SubApp from './ex01_components/SubApp';
-import SubApp from './ex02_route/SubApp';
+// import SubApp from './ex02_route/SubApp';
+import SubApp from './ex03_link/SubApp';
 
 function App() {
   return (
--- ch17_router/src/ex03_link/SubApp.jsx
+++ ch17_router/src/ex03_link/SubApp.jsx
@@ -3,24 +3,29 @@
 import Contact from "./Contact";
 import Home from "./Home";
 import Topics from "./Topics";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
 
+/**
+ * <a>태그 기능은 '서버로 주소 이동 요청' + Refresh(F5키)이다.
+ * 그래서 화면의 Refresh가 필요없을 때
+ * js에서 e.preventDefault()로 Refresh를 막는다.
+ * js를 사용하지 않고 보다 편리하게 다른 컴포넌트로 이동하되
+ * Refresh를 막기 위해 <Link>사용한다.
+ */
 const SubApp = () =>{
     return (
         <BrowserRouter>
             <div>
                 <h1>Hello React Router DOM</h1>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/topics">Topics</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/topics">Topics</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
-                {/* 위의 경로를 누르면 아래의 <Route>에 지정한 경로가 표현된다. */}
                 <Routes>
                     <Route path="/" element={<Home/>} />
                     <Route path="/topics" element={<Topics/>} />
                     <Route path="/contact" element={<Contact/>} />
-                    {/* 위의 주소를 제외한 나머지는 모두 아래에서 처리한다. */}
                     <Route path="/*" element={"Not Found"} />                    
                 </Routes>
             </div>
